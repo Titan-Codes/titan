5a5f643a6c73c3678a0c32f02909dc2cdbe0aa28
TITANxTCA <80444511+TITANxTCA@users.noreply.github.com>
2022-01-05 10:12:11 +0530

    added new transitions and decreased the number of particles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 50,
         "density": {
           "enable": true,
           "value_area": 800
