9329568e3b15f8934b04e59515f4ed9cf30baeec
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-05-30 19:21:24 +0530

    added a heading component.

---
+++ portfolio/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Heading from './Heading'
+
+function App(){
+    return (
+        <Heading />
+    )
+}
---
+++ portfolio/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Heading(){
+    return (
+        <h1>Hello, I'm TITAN</h1>
+    )
+}
+
+export default Heading;
