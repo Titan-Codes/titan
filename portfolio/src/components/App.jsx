5f0d634238eeb135532595fe627b0a3e38d94e08
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-01 12:42:20 +0530

    added navbar.

--- portfolio/src/components/App.jsx
+++ portfolio/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Heading from './Heading'
 import About from './About'
+import Navbar from "./Navbar";
 
 function App(){
     return (
         <div>
+            <Navbar />
             <Heading />
             <About />
         </div>
