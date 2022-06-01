d3b130e53a55addb449861db22d5d7c24327b5a7
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-01 15:11:56 +0530

    added social media icons and links.

--- portfolio/src/components/Icons.jsx
+++ portfolio/src/components/Icons.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Icons(){
     return (
         <div className="icons">
-            <a href="https://github.com/titan-codes" className="icon"><i class="socials fa-brands fa-github"></i></a>
-            <a href="https://github.com/titan-codes" className="icon"><i class="socials fa-brands fa-youtube"></i></a>
-            <a href="https://github.com/titan-codes" className="icon"><i class="socials fa-brands fa-discord"></i></a>
-            <a href="mailto:vedansh1sharma2005@gmail.com" className="icon"><i class="socials fa-solid fa-envelope"></i></a>
+            <a href="https://github.com/titan-codes" target="_blank" className="icon"><i class="socials fa-brands fa-github"></i></a>
+            <a href="https://github.com/titan-codes" target="_blank" className="icon"><i class="socials fa-brands fa-youtube"></i></a>
+            <a href="https://github.com/titan-codes" target="_blank" className="icon"><i class="socials fa-brands fa-discord"></i></a>
+            <a href="mailto:vedansh1sharma2005@gmail.com" target="_blank" className="icon"><i class="socials fa-solid fa-envelope"></i></a>
         </div>
     )
 }
