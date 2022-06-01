978114d4a5ae33742766bc39778f9c817726a99d
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-01 14:15:06 +0530

    changed the bg color and fonts.

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function About(){
     return (
         <section className="second-section" id="about">
-            <h1 className="about-heading"><i class="fa-solid fa-user"></i> About me</h1>
+            <div className="about-container">
+                <h1 className="about-heading"><i class="fa-solid fa-user"></i> About me</h1>
+                <br></br>
+                <p>Hello, I'm Vedansh Sharma AKA Titan and I'm 17 years old Python, Full Stack Developer.</p>
+            </div>
+            
         </section>
     )
 }
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,31 +2,32 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-dark navbar-expand-lg bg-dark sticky-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">TITAN</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tech Stack</a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark">
+            <a class="navbar-brand" href="#">TITAN</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tech Stack</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
 }
+
+
+// 
 
 export default Navbar;
