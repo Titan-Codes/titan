addd7573396ed9e6474206e3007b5d09fc197da3
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-01 12:01:11 +0530

    added icons.

--- portfolio/src/components/Heading.jsx
+++ portfolio/src/components/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Heading(){
     return (
         <section className="first-section" id="home">
-            <h1 className="heading">Hello, I'm TITAN</h1>
+            <h1 className="heading"><i class="fa-solid fa-terminal"></i> Hello, I'm TITAN</h1>
         </section>
     )
 }
