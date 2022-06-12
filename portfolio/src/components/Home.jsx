7d06a61a227df07323b3b7a5822d49feca5b7227
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-12 14:40:22 +0530

    added text and fonts.

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home(){
         <section id="home" className="home">
             <div className="home-section">
                 <h1 className="home-heading">Full-Stack Developer, Python Programmer</h1>
+                <p className="home-text">I make websites and code stuff with python and I love to code.</p>
             </div>
         </section>
     )
