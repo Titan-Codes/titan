571b6209c5ca94861281059d94d9ce2db5e0b368
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-12 17:36:37 +0530

    changed the text.

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home(){
         <section id="home" className="home">
             <div className="home-section">
                 <h1 className="home-heading">Full-Stack Developer, Python Programmer</h1>
-                <p className="home-text">I make websites and code stuff with python and I love to code.</p>
+                <p className="home-text">I make websites, code stuff with python and I love to code.</p>
                 <img className="avatar" src={imageUrl} />
             </div>
         </section>
