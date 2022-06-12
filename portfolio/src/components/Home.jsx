e5113091637852fb53721c8103a04fadf55fb698
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-12 12:42:19 +0530

    styling navbar from last 2 days :(

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Home(){
     return (
-        <h1 className="hero is-centered">I'm a Python Programmer and Fullstack Developer</h1>
+        <section className="home">
+            <h1 className="hero is-centered">I'm a Python Programmer and Fullstack Developer</h1>
+        </section>
     )
 }
 
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import React from "react";
 function Navbar() {
   const imageUrl = "https://cdn.discordapp.com/attachments/743817386792058971/984121645352517702/myAvatarCircle.png";
 
+  const checkbox = document.querySelector("input[name=checkbox]");
+  const items = document.querySelector(".items");
+
   return (
     <nav className="navbar">
       <div className="logo navbar-brand navbar-text" style={{color: "black"}}>
@@ -10,7 +13,11 @@ function Navbar() {
       </div>
       <ul className="navlinks">
         <input type="checkbox" id="checkbox_toggle" />
-        <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+        <label for="checkbox_toggle" class="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </label>
         <div className="menu">
           <li><a className="items navbar-text">Home</a></li>
           <li><a className="items navbar-text">About</a></li>
