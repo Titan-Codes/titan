0724cbc3f3b6d3886576d5f15d35bc785312702b
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-13 20:57:58 +0530

    fixed some typos.

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -6,7 +6,7 @@ function About(){
         <div className="about-section">
             <h1 className="about-heading">Hi, I'm Vedansh. Good to see you.</h1>
             <div className="text-container">
-                <p className="about-text">Thanks for checking in, my name is Vedansh Sharma and I'm 17 years old programmer <br /> who loves creating cool stuff. I fell in love with coding in 2020.<br /> Right now I'm in stu<span className="dying">dying</span> in grade 12th but my priority is always coding.</p>
+                <p className="about-text">Thanks for checking in, my name is Vedansh Sharma and I'm 17 years old programmer <br /> who loves creating cool stuff. I fell in love with coding in 2020.<br /> Right now I'm stu<span className="dying">dying</span> in grade 12th but my priority is always coding.</p>
             </div>
             
         </div>   
